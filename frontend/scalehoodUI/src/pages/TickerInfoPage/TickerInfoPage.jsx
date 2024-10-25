c15b3fc0aa97a6934e7bdde2a834f573052bf18c
L-E-A-R-N-E-R <porwalmohit1999@gmail.com>
2024-10-25 11:53:38 -0400

    Added API call in Ticker Info page to fetch tickerinfo data

--- frontend/scalehoodUI/src/pages/TickerInfoPage/TickerInfoPage.jsx
+++ frontend/scalehoodUI/src/pages/TickerInfoPage/TickerInfoPage.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useEffect, useState } from 'react';
 import Graph from '../../components/TickerInfo/Graph/Graph';
 import About from '../../components/TickerInfo/About/About';
 import CompanyInfo from '../../components/TickerInfo/CompanyInfo/CompanyInfo';
@@ -7,15 +8,44 @@ import { useParams } from 'react-router-dom';
 
 export default function TickerInfoPage(){
 
+    const[tickerData, setTickerData] = useState(null);
+
     const { ticker } = useParams();
     
+    //Fetch ticker info data from the DB when component mounts
+    useEffect( () => {
+        const fetchTickerInfo = async () => {
+            console.log("Fetch ticker info has been called");
+            try{
+                const response = await fetch(`http://127.0.0.1:5000/tickerInfo/${ticker}`);
+                const data = await response.json();
+                console.log("Data "+ JSON.stringify(data));
+                setTickerData(data);
+            }catch(error){
+                console.error("Error fetching data", error);
+            }
+        };
+        fetchTickerInfo();
+    }, [ticker]);
+
     return(
         <div>
-            <Header/>
+            <Header currentPrice={tickerData.current_price}/>
             <Graph/>
-            <About/>
-            <CompanyInfo/>
+            <About summary={tickerData.summary}/>
+            <CompanyInfo 
+            marketcap={tickerData.marketcap} 
+            fullTimeEmployees={tickerData.fulltime_employees}
+            ceo={tickerData.ceo}
+            headquarters={tickerData.headquarters}
+            dividendYield={tickerData.dividendYield}
+            averageVolume={tickerData.avg_volume}
+            earningsGrowth={tickerData.earnings_growth}
+            grossMargins={tickerData.gross_margins}
+            />
         </div>
     )
 }
+
+
 
